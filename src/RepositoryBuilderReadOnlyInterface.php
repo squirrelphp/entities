3527d6c1e39be979ce77f40fc2e12ea152002a37
Andreas Leathley <andreas.leathley@panaxis.ch>
2019-08-20 13:32:54 +0200

    Fix psalm complaint with return type

--- src/RepositoryBuilderReadOnlyInterface.php
+++ src/RepositoryBuilderReadOnlyInterface.php
@@ -10,6 +10,8 @@ interface RepositoryBuilderReadOnlyInterface
      * Returns class Squirrel\Entities\Action\SelectEntries as a SELECT query builder,
      * but we omit a return docblock to avoid confusion and errors in linting, because when
      * repositories are generated we specify the exact return class
+     *
+     * @return mixed
      */
     public function select();
 
